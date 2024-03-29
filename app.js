6d185ff43479959bf15fb20e89c62be9de1e58ff
GautamSolar <165277143+GautamSolar@users.noreply.github.com>
2024-03-29 10:35:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const {PersonRouter} = require('./Routes/Person.Route')
 const {designationRouter} = require('./Routes/DesignationRoute')
 const {IQCSolarCellRoute} = require('./Routes/IQCSolarCellRoute')
 const app = express()
+const PORT = process.env.PORT || 8080
 const cors = require('cors')
-
+require('dotenv').config()
 app.use(express.json())
 app.use(cors())
+
 
 
 
@@ -24,7 +26,7 @@ app.use('/QCM',designationRouter)
 /** to IQC Solar Cell */
 app.use('/IQCSolarCell',IQCSolarCellRoute)
 
-app.listen(5000,async()=>{
+app.listen(PORT,async()=>{
   try{
  dbConn
   console.log('server is running')
